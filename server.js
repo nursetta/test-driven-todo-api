b1777b26e95f0a06ef3b75a8e144e3d40b3ec16f
nursetta <n_ursetta@yahoo.com>
2016-07-07 14:51:32 -0600

    original

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+   { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+   { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+   { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -50,21 +50,32 @@ app.get('/api/todos/search', function search(req, res) {
 });
 
 app.get('/api/todos', function index(req, res) {
-  /* This endpoint responds with all of the todos
-   */
+  res.json({todos: todos});
+
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+  var newTodo = req.body;
+  var nextId = (todos.length + 1);
+    newTodo._id = nextId;
+    todos.push(newTodo);
+    res.json(newTodo);
+
 });
 
 app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
-   * id specified in the route parameter (:id)
-   */
-});
+
+  var id = parseInt(req.params.id);
+  var todoWeWant = todos.filter(function(todo) {
+      return (id === todo['_id']);
+  })[0];
+
+  res.json(todoWeWant);
+
+});  
 
 app.put('/api/todos/:id', function update(req, res) {
   /* This endpoint will update a single todo with the
@@ -78,6 +89,14 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with deleted todo.
    */
+   var deleteTodoID = req.params.id;
+     for( var i = 0; i<todos.length; i++){
+       if (todos[i]._id == deleteTodoID){
+           todos.splice(i, 1);
+           res.json({todos: "delete"});
+       }
+     }
+
 });
 
 /**********
